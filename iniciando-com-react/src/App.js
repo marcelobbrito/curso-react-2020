9aba09e766fac802f6cddee167abdb2e40cf3784
Marcelo <marcelobbrito@gmail.com>
2020-08-05 10:06:19 -0300

    UPDATES

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component{
+
+  state = {
+    nome: ''
+  }
+
+  moodificarNome = (event) => {
+      this.setState({
+        nome: event.target.value
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <input type="text" value={this.state.nome} onChange={this.moodificarNome} />
+        <h1>{this.state.nome}</h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+function App2() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
     </div>
   );
 }
